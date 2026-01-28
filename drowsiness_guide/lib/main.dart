6943d470c49e9d2a036ba5f026789b918557da72
PJ-004 <pranavjworkacc@gmail.com>
2026-01-28 00:18:54 -0800

    Added drowsiness_guide Flutter app

---
+++ drowsiness_guide/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('It took me longer to figure out dependencies than to compile this goddamned app'),
+        ),
+      ),
+    );
+  }
+}
